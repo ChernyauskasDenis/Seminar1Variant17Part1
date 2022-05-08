82dd076845ba03c555be3f6d83e15883b9553542
Xopop <Xopop@KADZUKI>
2022-05-09 00:59:05 +0300

    Добавьте файлы проекта.

---
+++ Seminar1Variant17Part1.cpp
@@ -0,0 +1,31 @@
+﻿//Семинар 1 вариант 17, часть 1
+
+#include <iostream>
+#include <cmath>
+#include <locale.h>
+
+
+
+int main()
+{
+	{} {}
+	double m, z, a, b, x, c;
+	setlocale(LC_ALL, "Rus");
+	std::cout << "Введите m \n";
+	std::cin >> m;
+	if (m <= 0)
+	{
+		std::cout << "Функция не имеет значений \n";
+	}
+	else
+	{
+		x = 3 * m + 2;
+		a = (sqrt(pow(x, 2) - 24 * m));
+		c = sqrt(m);
+		b = (3 * (sqrt(m)) - 2 / c);
+		z = a / b;
+		std::cout << "результат  =  " << z;
+	}
+	return 0;
+
+}
